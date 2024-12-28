c5df3b52fcb942e1d854ed41ca55ecf2a6a2c056
Fatoom Fayad <fatoomfayyad@gmail.com>
2024-12-28 14:18:18 +0200

    Add files via upload
    strings and numbers

---
+++ avg_three_numbers.py
@@ -0,0 +1,6 @@
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+third_num = int(input("Enter third number: "))
+
+avg = (first_num + second_num + third_num) / 3
+print(avg)
---
+++ main.py
@@ -0,0 +1,7 @@
+myStr = "Just do it!"
+print(myStr[-1])
+print(myStr[5:7])
+print(myStr[-3:])
+print(myStr[:4])
+
+print("Don't " + myStr[-6:])
