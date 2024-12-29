6fa1662faba67d068e1dbdf77a61d82827213a8b
Fatoom Fayad <fatoomfayyad@gmail.com>
2024-12-29 11:31:32 +0200

    concat three text from user input

---
+++ text_concat.py
@@ -0,0 +1,7 @@
+from cgi import print_form
+
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favourite color?")
+
+print("So your name is" ,  name ,", your quest is", quest, ", and your favorite color is ", color, ".")
