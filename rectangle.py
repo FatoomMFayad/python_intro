43bc33380a566add3e991b723d03a4743d73387b
Fatoom Fayad <fatoomfayyad@gmail.com>
2024-12-29 13:07:05 +0200

    rectangle area and perimeter

---
+++ rectangle.py
@@ -0,0 +1,6 @@
+width = float(input("Enter the width of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+
+area = width * height
+perimeter  = 2 * (width + height)
+print(f"The area of the rectangle = {area} \nPerimeter  of the rectangle = {perimeter}")
